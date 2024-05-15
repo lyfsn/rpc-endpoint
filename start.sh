567bd2675e15751881d4322d03ab8bf93deaf539
lyfsn <dev.wangyu@proton.me>
2024-05-15 15:27:09 +0800

    up

---
+++ start.sh
@@ -0,0 +1,7 @@
+
+
+go run cmd/server/main.go \
+  -redis 95.217.233.186:6379 \
+  -signingKey 0x82db12be5d5ba9b278edb01fea675a9f6a95fcad35a9b72d2a872d8505c3e9ba \
+  -proxy http://95.217.233.186:9062
+
